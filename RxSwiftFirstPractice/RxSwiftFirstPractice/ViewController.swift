8d5e458c0f18d830b1f7c3c0225be4c0486405b4
Ken20215 <k.ishioka@renk.co.jp>
2024-08-27 11:18:01 +0900

    挙動のコメント追加

--- RxSwiftFirstPractice/RxSwiftFirstPractice/ViewController.swift
+++ RxSwiftFirstPractice/RxSwiftFirstPractice/ViewController.swift
@@ -14,7 +14,7 @@ final class ViewController: UIViewController {
     
     private let disposeBag = DisposeBag()
     private var viewModel = ViewModel()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         bind()
@@ -23,12 +23,34 @@ final class ViewController: UIViewController {
 
 extension ViewController {
     func bind() {
+        
+        /*
+        ここがSubscriber（サブスクライバー（購買者））と呼ばれており、実際に商品を購入する方々のイメージ。
+        ここでは新商品が来るたびに通知され、その情報を受け取る役割があります。
+         */
         viewModel.hellorWorldObservable
+        
+        /*
+         実際にここで購入の手続きを行うイメージです。ここがいわゆるサブスクライバーと言われる挙動になります。
+         手続きはonNextの他、onCompletedやonDisposedがありますが、これらは省略することが可能でonNextのみ記述でOKなものです。
+         MARK: ここではObservableが新しくデータを発行し、その通知が問答無用でデータを受け取るイメージです。
+         */
             .subscribe(onNext: { value in
                 print("value = \(value)")
             })
             .disposed(by: disposeBag)
+        
+        /*
+         ここではsubscribeのonNextでイベントの登録を行い、実際にイベントの発生をさせるviewModel.updateItem()を行なってます。
+         これをイベントが発生する前に行なってしまうとデータを上手く取得できません。登録が先に行われ、その後にイベントが発生する必要があります。
+         
+         これを例えると
+         ↓
+         MARK: 「友達が結婚式に行かないのに招待状を送っても意味がないのに、その手順を指定しているのでおかしくね？」となってます。
+         MARK: また商品を購入しないのに購入手続きをしてしまうと押売りのような形になってしまうので、システムがデータを上手く取れない現象が起きてます。
+         */
         viewModel.updateItem()
+
     }
 }
 
--- RxSwiftFirstPractice/RxSwiftFirstPractice/ViewModel.swift
+++ RxSwiftFirstPractice/RxSwiftFirstPractice/ViewModel.swift
@@ -11,12 +11,27 @@ import RxSwift
 import RxCocoa
 
 final class ViewModel {
+    /*
+    Observable<String>はお店の掲示板のような存在です。
+    お客はこの情報を元に商品購入して行きます。ここでは掲示板に文字（String型）が表示されています。
+    */
     var hellorWorldObservable: Observable<String> {
+        /*
+        hellorWorldObservable変数はhelloSubject（商品）をasObservable(ショーウィンドウ)で陳列させるイメージ。
+        商品情報を流すことはしますが、サブスクライバー（お客）が外部から直接操作をされたり商品に触れられたくない時に使います。
+        例えるとお店の倉庫に入り勝手に商品を取られたくないようしたい時に実装します。
+         */
         return helloSubject.asObservable()
     }
-    
+    /*
+    helloSubjectはSubjectでString型のお店のような存在で、商品を棚に並べる役割で、この棚に商品を置いていくイメージです。
+    商品を置く挙動は下の.onNextのようにHello Worldを追加する感じです。
+     */
     private let helloSubject = PublishSubject<String>()
     
+    /*
+    実際にイベントを発行する挙動をここで登録。
+     */
     func updateItem() {
         helloSubject.onNext("Hello World")
         helloSubject.onNext("Hello World!")
