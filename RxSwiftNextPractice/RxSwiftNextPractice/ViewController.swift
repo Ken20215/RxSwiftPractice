41bc8e01def02f92aaff7ec0128ce845e6b53d00
Ken20215 <k.ishioka@renk.co.jp>
2024-08-27 11:43:36 +0900

    pod追加

---
+++ RxSwiftNextPractice/RxSwiftNextPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftNextPractice
+//
+//  Created by *石岡顕* on 2024/08/27.
+//  Copyright (c) 2024 *ReNKCHANNEL*. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
