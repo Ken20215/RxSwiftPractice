6f08516ac0bda8ff3ab3c8b9d12d85470e976f6e
Ken20215 <k.ishioka@renk.co.jp>
2024-08-27 13:02:54 +0900

    コード追加

--- RxSwiftNextPractice/RxSwiftNextPractice/ViewController.swift
+++ RxSwiftNextPractice/RxSwiftNextPractice/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var hogeButton: UIButton!
+    @IBOutlet private weak var fooButton: UIButton!
+    private let disposeBag = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        bind()
     }
-
-
 }
 
+extension ViewController {
+    func bind() {
+        hogeButton.rx.tap
+            .subscribe(onNext: {
+                print("hogeButtonのsubscribe発火☀️")
+            })
+            .disposed(by: disposeBag)
+        
+        fooButton.rx.tap
+            .subscribe(onNext: {
+                print("fooButtonのsubscribe発火☔️")
+            })
+            .disposed(by: disposeBag)
+    }
+}
