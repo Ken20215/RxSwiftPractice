ac7b3a0ab2ec8b0523fdef11f34bf13627ff487f
Ken20215 <k.ishioka@renk.co.jp>
2024-08-27 13:51:56 +0900

    コメント追加

--- RxSwiftNextPractice/RxSwiftNextPractice/ViewController.swift
+++ RxSwiftNextPractice/RxSwiftNextPractice/ViewController.swift
@@ -24,15 +24,24 @@ class ViewController: UIViewController {
 
 extension ViewController {
     func bind() {
-        hogeButton.rx.tap
+        /*
+         *** MARK: ObservableとObserverの違いについて ***
+         イベント発生元がObservableで、イベントの処理を行うのがObserverです。
+         */
+        hogeButton.rx.tap // Observable（イベントの発生元）
             .subscribe(onNext: {
-                print("hogeButtonのsubscribe発火☀️")
+                print("hogeButtonのsubscribe発火☀️") // Observer（イベントの処理）
             })
+        /* 
+         *** MARK: disposedの役割について ***
+         ここでは購買をやめる手続きを行います。一度始めた購入を止め、不要なものを処分する手続きです。
+         もう少しイメージを膨らますと、欲しい物を購入する際に「もういらないのでお会計をする」ようなイメージです。
+         */
             .disposed(by: disposeBag)
         
-        fooButton.rx.tap
+        fooButton.rx.tap // Observable（イベントの発生元）
             .subscribe(onNext: {
-                print("fooButtonのsubscribe発火☔️")
+                print("fooButtonのsubscribe発火☔️") // Observer（イベントの処理）
             })
             .disposed(by: disposeBag)
     }
