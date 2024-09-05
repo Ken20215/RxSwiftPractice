cf6767eca29cfb882efebdb74271411039791856
Ken20215 <k.ishioka@renk.co.jp>
2024-09-05 14:33:07 +0900

    立ち上げ

---
+++ RxSwiftStopWhatchApp/RxSwiftStopWhatchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftStopWhatchApp
+//
+//  Created by *石岡顕* on 2024/09/05.
+//  Copyright (c) 2024 *ReNKCHANNEL*. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
