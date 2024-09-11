5649210654a34b3bc0f3807a46e42548b8e5caf5
Ken20215 <k.ishioka@renk.co.jp>
2024-09-11 17:16:09 +0900

    pickerの変更

--- RxSwiftStopWhatchApp/RxSwiftStopWhatchApp/TimerViewController.swift
+++ RxSwiftStopWhatchApp/RxSwiftStopWhatchApp/TimerViewController.swift
@@ -16,8 +16,9 @@ final class TimerViewController: UIViewController {
     @IBOutlet private weak var startButton: UIButton!
     @IBOutlet private weak var stopButton: UIButton!
     @IBOutlet private weak var resetButton: UIButton!
-    @IBOutlet private weak var timePicker: UIPickerView!
     @IBOutlet private weak var countLabel: UILabel!
+    @IBOutlet private weak var selectTimeButton: UIButton!
+    
     let settingArray = [10, 20, 30, 40, 50, 60]
     let settingKey = "timer_value"
     let viewModel = TimerViewModel()
@@ -25,8 +26,6 @@ final class TimerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        timePicker.delegate = self
-        timePicker.dataSource = self
         bind(to: viewModel)
         timeSetting()
     }
@@ -36,6 +35,12 @@ private extension TimerViewController {
     func bind(to viewModel: TimerViewModel) {
         startButton.rx.tap
             .bind(to: viewModel.input.startButton)
+            .disposed(by: disposeBag)
+        
+        selectTimeButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.showTimePicker()
+            })
             .disposed(by: disposeBag)
         
         stopButton.rx.tap
@@ -52,41 +57,33 @@ private extension TimerViewController {
         
     }
     
+    func showTimePicker() {
+        let alertController = UIAlertController(title: "時間を選択", message: nil, preferredStyle: .actionSheet)
+        
+        for time in settingArray {
+            let action = UIAlertAction(title: "\(time)秒", style: .default) { [weak self] _ in
+                self?.updateTimeSelection(with: time)
+            }
+            alertController.addAction(action)
+        }
+        
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func updateTimeSelection(with time: Int) {
+        countLabel.text = "残り\(time)秒"
+        UserDefaults.standard.set(time, forKey: settingKey)
+        viewModel.input.countdownTime.accept(TimeInterval(time))
+    }
+    
     func timeSetting() {
         let settings = UserDefaults.standard
         let timerValue = settings.integer(forKey: settingKey)
         let initialTime = timerValue != 0 ? timerValue : 10
         countLabel.text = "残り\(initialTime)秒"
         viewModel.input.countdownTime.accept(TimeInterval(initialTime))
-        
-        for row in 0..<settingArray.count {
-            if settingArray[row] == initialTime {
-                timePicker.selectRow(row, inComponent: 0, animated: true)
-            }
-        }
     }
-}
-
-extension TimerViewController: UIPickerViewDataSource, UIPickerViewDelegate {
-    
-    // pickerの列を指定
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    // pickerの行数を指定
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return settingArray.count
-    }
-    // pickerの中身を表示させる内容を指定
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return String(settingArray[row])
-    }
-    // pickerの選択時に実行
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let selectedValue = settingArray[row]
-        countLabel.text = "残り\(selectedValue)秒"
-        UserDefaults.standard.set(selectedValue, forKey: settingKey)
-        viewModel.input.countdownTime.accept(TimeInterval(selectedValue))
-    }
-    
 }
