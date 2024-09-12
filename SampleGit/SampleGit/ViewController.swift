edf5558488f484e51fffff123ed9368c55612b26
Ken20215 <k.ishioka@renk.co.jp>
2024-09-12 15:06:33 +0900

    練習ブランチ

---
+++ SampleGit/SampleGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleGit
+//
+//  Created by *石岡顕* on 2024/09/12.
+//  Copyright (c) 2024 *ReNKCHANNEL*. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
