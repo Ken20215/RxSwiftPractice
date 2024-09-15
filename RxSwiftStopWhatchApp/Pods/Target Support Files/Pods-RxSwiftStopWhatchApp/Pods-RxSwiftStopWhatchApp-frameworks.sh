9d2dd94164389623a6b8e5a46f1421a59ec68c18
Ken20215 <k.ishioka@renk.co.jp>
2024-09-15 11:06:15 +0900

    コード修正

--- RxSwiftStopWhatchApp/Pods/Target Support Files/Pods-RxSwiftStopWhatchApp/Pods-RxSwiftStopWhatchApp-frameworks.sh
+++ RxSwiftStopWhatchApp/Pods/Target Support Files/Pods-RxSwiftStopWhatchApp/Pods-RxSwiftStopWhatchApp-frameworks.sh
@@ -176,13 +176,11 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/ActionSheetPicker-3.0/ActionSheetPicker_3_0.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/ActionSheetPicker-3.0/ActionSheetPicker_3_0.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
