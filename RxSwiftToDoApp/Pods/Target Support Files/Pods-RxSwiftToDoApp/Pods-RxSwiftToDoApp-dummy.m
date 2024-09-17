2e46e22cec6aa3a577eafc91538d2191cc19fb3c
Ken20215 <k.ishioka@renk.co.jp>
2024-09-17 17:42:00 +0900

    環境構築

---
+++ RxSwiftToDoApp/Pods/Target Support Files/Pods-RxSwiftToDoApp/Pods-RxSwiftToDoApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftToDoApp : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftToDoApp
+@end
---
+++ RxSwiftToDoApp/RxSwiftToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftToDoApp
+//
+//  Created by *石岡顕* on 2024/09/17.
+//  Copyright (c) 2024 *ReNKCHANNEL*. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
