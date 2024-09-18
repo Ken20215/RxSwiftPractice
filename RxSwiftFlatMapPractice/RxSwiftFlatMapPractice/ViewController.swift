27d9412006104130846f50700573e49fbf22e5af
Ken20215 <k.ishioka@renk.co.jp>
2024-09-18 14:05:55 +0900

    flatMapを使ってみた

--- RxSwiftFlatMapPractice/RxSwiftFlatMapPractice/ViewController.swift
+++ RxSwiftFlatMapPractice/RxSwiftFlatMapPractice/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private weak var displayLabel: UILabel!
+    @IBOutlet private weak var firstTapButton: UIButton!
+    @IBOutlet private weak var secondTapButton: UIButton!
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        bind()
     }
-
-
 }
 
+extension ViewController {
+    func bind() {
+        let buttanTapA = firstTapButton.rx.tap.map { "ファーストボタンが押されました。" }
+        let buttanTapB = secondTapButton.rx.tap.map { "セカンドボタンが押されました。" }
+        Observable.of(buttanTapA, buttanTapB)
+            .flatMap { $0 }
+            .asDriver(onErrorJustReturn: "データないよ")
+            .drive(displayLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+}
