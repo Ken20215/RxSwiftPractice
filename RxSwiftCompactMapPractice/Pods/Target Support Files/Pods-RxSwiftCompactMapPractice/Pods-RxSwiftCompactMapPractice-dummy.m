3cb3db95dc56fbf21b55b448ac474d5a552a8495
Ken20215 <k.ishioka@renk.co.jp>
2024-09-19 13:40:29 +0900

    pod追加

---
+++ RxSwiftCompactMapPractice/Pods/Target Support Files/Pods-RxSwiftCompactMapPractice/Pods-RxSwiftCompactMapPractice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftCompactMapPractice : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftCompactMapPractice
+@end
---
+++ RxSwiftCompactMapPractice/RxSwiftCompactMapPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftCompactMapPractice
+//
+//  Created by *石岡顕* on 2024/09/19.
+//  Copyright (c) 2024 *ReNKCHANNEL*. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
