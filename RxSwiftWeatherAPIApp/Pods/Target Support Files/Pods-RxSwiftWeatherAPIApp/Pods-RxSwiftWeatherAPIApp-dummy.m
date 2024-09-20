0e85486d5c87de83f651cc2f70cdeed142e6dc54
Ken20215 <k.ishioka@renk.co.jp>
2024-09-20 11:45:39 +0900

    pod追加

---
+++ RxSwiftWeatherAPIApp/Pods/Target Support Files/Pods-RxSwiftWeatherAPIApp/Pods-RxSwiftWeatherAPIApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftWeatherAPIApp : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftWeatherAPIApp
+@end
---
+++ RxSwiftWeatherAPIApp/RxSwiftWeatherAPIApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftWeatherAPIApp
+//
+//  Created by *石岡顕* on 2024/09/20.
+//  Copyright (c) 2024 *ReNKCHANNEL*. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
